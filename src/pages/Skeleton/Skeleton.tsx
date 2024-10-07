30d246cb5bd0567d47b08af303fccca8c3709e9c
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-10-07 20:19:30 -0300

    :sparkles: refact skeleton

--- src/pages/Skeleton/Skeleton.tsx
+++ src/pages/Skeleton/Skeleton.tsx
@@ -1,57 +1,119 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { TransformWrapper, TransformComponent, ReactZoomPanPinchRef } from "react-zoom-pan-pinch";
 import './Skeleton.scss';
+import { useRef, useState } from 'react';
+import skeletonPng from '../../assets/skeleton.png';
+import joelhoPng from '../../assets/patela.png';
+import { Button } from "@/components/ui/button"
 
 const Skeleton = () => {
+  const transformComponentRef = useRef<ReactZoomPanPinchRef | null>(null);
+  const [isOssos, setIsOssos] = useState(true);
+
+  const zoomToJoelho = () => {
+    if (transformComponentRef.current) {
+      const { zoomToElement } = transformComponentRef.current;
+      zoomToElement("joelho");
+    }
+  };
+
+  const zoomOut = () => {
+    if(transformComponentRef.current){
+      const { zoomOut } = transformComponentRef.current
+      zoomOut()
+    }
+  }
+
+  const zoomIn = () => {
+    if(transformComponentRef.current){
+      const { zoomIn } = transformComponentRef.current
+      zoomIn()
+    }
+  }
+
+  const resetZoom = () => {
+    if(transformComponentRef.current){
+      const { resetTransform } = transformComponentRef.current
+      resetTransform()
+    }
+  }
   return (
     <section className="home_container">
       <nav className="sidebar_container">
         <ul>
-          <h4>Generators</h4>
-            <NavLink to='/' className={({ isActive }) => (isActive ? 'active' : '')}>
-              <li>
-                <span className="material-symbols-outlined">user_attributes</span>
-                Person
-              </li>
-            </NavLink>
-          
-            <NavLink to='cpf' className={({ isActive }) => (isActive ? 'active' : '')}>
-              <li>
-                <span className="material-symbols-outlined">id_card</span>
-                Cpf
-              </li>
-            </NavLink>
+          <h4>Ossos</h4>
+          <li onClick={zoomToJoelho}>
+            <span className="material-symbols-outlined">user_attributes</span>
+            Osso 1
+          </li>
+      
+          <li onClick={zoomToJoelho}>
+            <span className="material-symbols-outlined">user_attributes</span>
+            Osso 2
+          </li>
         </ul>
         <span className='divider'></span>
         <ul>
-          <h4>Text</h4>
-          <li>
-            <span className="material-symbols-outlined">text_fields</span>
-            Transform
+          <h4>Músculos</h4>
+          <li onClick={zoomToJoelho}>
+            <span className="material-symbols-outlined">user_attributes</span>
+            Músculo 1
+          </li>
+          <li onClick={zoomToJoelho}>
+            <span className="material-symbols-outlined">user_attributes</span>
+            Músculo 2
+          </li>
+          <li onClick={zoomToJoelho}>
+            <span className="material-symbols-outlined">user_attributes</span>
+            Músculo 3
           </li>
         </ul>
+        <span className='divider'></span>
         <ul>
-          <h4>Others</h4>
-          <NavLink to='public-apis' className={({ isActive }) => (isActive ? 'active' : '')}>
-            <li>
-              <span className="material-symbols-outlined">integration_instructions</span>
-              Public APIs
-            </li>
-          </NavLink>
-          <NavLink to='icons' className={({ isActive }) => (isActive ? 'active' : '')}>
-            <li>
-              <span className="material-symbols-outlined">mood</span>
-              Icons
-            </li>
-          </NavLink>
-          <li>
-            <span className="material-symbols-outlined">image</span>
-            Free images
+          <h4>Ligamentos</h4>
+          <li onClick={zoomToJoelho}>
+            <span className="material-symbols-outlined">user_attributes</span>
+            Ligamento 1
+          </li>
+          <li onClick={zoomToJoelho}>
+            <span className="material-symbols-outlined">user_attributes</span>
+            Ligamento 2
           </li>
         </ul>
       </nav>
       
       <main className="main_container">
- 
+        <div className='controls-wrapper'>
+          <Button className='button-control' onClick={zoomIn}>Zoom in +</Button>
+          <Button className='button-control' onClick={zoomOut}>Zoom out -</Button>
+          <Button className='button-control' onClick={resetZoom}>Resetar</Button>
+        </div>
+        <span onClick={() => setIsOssos(!isOssos)} className="material-symbols-outlined">cached</span>
+        {isOssos &&<TransformWrapper
+          initialScale={1}
+          ref={transformComponentRef}
+        >
+          <TransformComponent>
+            <img className='bg-yellow-300 skeleton-full' src={skeletonPng} alt="Esqueleto do corpo humano" />
+            <Link id='joelho' className='joelho' to="/knee">
+              <img className='joelho-patela' src={joelhoPng} alt="" />
+            </Link>
+
+          </TransformComponent>
+        </TransformWrapper>}
+
+        {!isOssos && <TransformWrapper
+          initialScale={1}
+          ref={transformComponentRef}
+        >
+          <TransformComponent>
+            <img className='bg-lime-300 skeleton-full' src={skeletonPng} alt="Esqueleto do corpo humano" />
+            <Link id='joelho' className='joelho' to="/rota1">
+              <img className='joelho-patela' src={joelhoPng} alt="" />
+            </Link>
+
+          </TransformComponent>
+        </TransformWrapper>}
       </main>
     </section>
   )
