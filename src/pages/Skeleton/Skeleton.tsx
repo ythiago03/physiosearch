16a21eb4fdb34bd302f2ff4229719869f677f3d2
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-10-22 18:38:17 -0300

    :sparkles: add loading in dialog images

--- src/pages/Skeleton/Skeleton.tsx
+++ src/pages/Skeleton/Skeleton.tsx
@@ -17,14 +17,15 @@ import { Separator } from '@/components/ui/separator';
 import data from "../../services/fakeApi.ts"
 import { Link } from 'react-router-dom';
 import { TransformWrapper, TransformComponent, ReactZoomPanPinchRef } from "react-zoom-pan-pinch";
-import { useRef } from 'react';
-
+import { useRef, useState } from 'react';
 import skeletonPng from '../../assets/skeleton.png';
+import { Skeleton as SkeletonShadCn } from "@/components/ui/skeleton"
 
 import './Skeleton.scss';
 
 const Skeleton = () => {
   const transformComponentRef = useRef<ReactZoomPanPinchRef | null>(null);
+  const [isImageLoading, setIsImageLoading] = useState<boolean>(true);
 
   const zoomOut = () => {
     if(transformComponentRef.current){
@@ -47,6 +48,21 @@ const Skeleton = () => {
     }
   }
 
+  const loadImage = (imageSrc: string) => {
+    return new Promise<void>((resolve, reject) => {
+      const img = new Image();
+      img.src = imageSrc;
+      img.onload = () => resolve();
+      img.onerror = () => reject();
+    })
+  }
+
+  const getImage = async (src: string): Promise<void> => {
+    setIsImageLoading(true)
+    await Promise.resolve(loadImage(src))
+    setIsImageLoading(false)
+  }
+
   return (
     <section className="home_container">
       <main className="main_container">
@@ -67,7 +83,10 @@ const Skeleton = () => {
             { data.map(partOfBody => {
               return <Dialog key={partOfBody.id}>
               <DialogTrigger asChild>
-                <button className={partOfBody.areaName.toLowerCase()+'_btn'}>
+                <button 
+                  className={partOfBody.areaName.toLowerCase()+'_btn'}
+                  onClick={() => getImage('/assets/'+partOfBody.areaName.toLowerCase()+'.png')}
+                >
                   <img className={partOfBody.areaName.toLowerCase()} src={'/assets/'+partOfBody.areaName.toLowerCase()+'.png'} alt={'Foto do '+partOfBody.areaName} />
                 </button>
               </DialogTrigger>
@@ -79,6 +98,10 @@ const Skeleton = () => {
                     Clique sobre uma patologia para saber mais detalhes
                   </DialogDescription>
                 </DialogHeader>
+
+                { 
+                  isImageLoading && <SkeletonShadCn className="h-[250px] w-[90%] m-auto rounded-xl" />
+                }
 
                 <img className='m-auto w-[80%]' src={'/assets/'+partOfBody.areaName.toLowerCase()+'Detalhado.png'} alt="" />
                 <Accordion type="single" collapsible>
