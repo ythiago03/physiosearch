682aa58b8f0da932dd027dbf277e59c0cdd8df3f
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-10-31 20:38:17 -0300

    :sparkles: add content in articles

--- src/services/fakeApi.ts
+++ src/services/fakeApi.ts
@@ -38,24 +38,157 @@ const data: PartOfBody[] = [
     pathologies: [
       {
         id: "4Ei10T",
-        name: "Entorse",
+        name: "Síndrome patelofemoral",
         articles: [
           {
             id: "X9aK81",
-            title: "Entorse de Tornozelo",
-            timeOfRead: 2,
-            dateOfPublication: "10/10/2024",
-            link: "https://physiosearch.vercel.app",
+            title: "Physical Examination and Patellofemoral Pain Syndrome: an Updated Review",
+            timeOfRead: 3,
+            dateOfPublication: "29/10/2021",
+            link: "https://link.springer.com/article/10.1007/s12178-021-09730-7",
             resume: [
               {
                 paragraph: 0,
-                title: "Título 1",
-                content: "Lorem ipsum sillor dollor amet."  
+                title: "Purpose of Review",
+                content: "Patellofemoral pain syndrome (PFPS) accounts for 25 to 40% of all knee disorders. Diagnosis of PFPS is primarily based on history and physical examination, but the findings on physical examination are often subtle and do not consistently correlate with symptoms described. Yoon and Fredericson published a review article in 2006 detailing the physical examination maneuvers most frequently used to assist clinicians in the accurate diagnosis and treatment of PFPS, and our aim in this review is to provide an update on this previous article focusing on the literature published over the past 15 years regarding the topic."  
               },
               {
                 paragraph: 1,
-                title: "Título 2",
-                content: "Lorem ipsum sillor dollor amet."  
+                title: "Recent Findings",
+                content: "Since publication of Fredericson’s original review article, there have been studies building on the literature specifically surrounding Q angle, patellar tilt, crepitus, strength and functional testing, and physical examination maneuver clustering. Additionally, multiple studies have been conducted on the use of musculoskeletal ultrasound (US) as a diagnostic tool for PFPS."  
+              },
+              {
+                paragraph: 2,
+                title: "Summary",
+                content: "Recent literature has further supported Q angle (when measured utilizing a standardized protocol), crepitus, weakness of hip abductorsandextensors,andweaknessdetectedin functionaltestingaspredictorsofPFPSwhile findinginconsistentevidencebehindlateral patellar tilt as a predictor of PFPS. The reliability of most physical examination tests alone remain low, but clustering physical examination findings may provide better sensitivities and specificities in diagnosing PFPS. Musculoskeletal US is rapidly gaining popularity, and decreased vastus medialis obliquus (VMO) volume, asymmetry in gluteus medius thickness, intra-articular effusions, and quadriceps andpatellartendonthicknesseshave shown valuein diagnosingthosewithPFPS.Additionally,US hastheadvantageof providingdynamic examination as well as evaluation of the patellofemoral joint in newborns and infants as a predictor of future patellofemoral instability. Further studies are needed to establish the gold standard for diagnosing PFPS and what US findings are truly predictive of PFPS."  
+              }
+            ]
+          },
+        ],
+        treatment:[
+          {
+            id: "DWnZHq",
+            title: "A Contemporary Approach to Patellofemoral Pain in Runners",
+            timeOfRead: 2,
+            dateOfPublication: "19/11/2020",
+            link: "https://meridian.allenpress.com/jat/article/55/12/1206/447918/A-Contemporary-Approach-to-Patellofemoral-Pain-in?searchresult=1",
+            resume: [
+              {
+                paragraph: 0,
+                title: "Purpose of Review",
+                content: "Patellofemoral pain (PFP) is among the most common injuries in recreational runners. Current evidence does not identify alignment, muscle weakness, and patellar maltracking or a combination of these as causes of PFP. Rather than solely investigating biomechanics, we suggest a holistic approach to address the causes of PFP. Both external loads, such as changes in training parameters and biomechanics, and internal loads, such as sleep and psychological stress, should be considered. As for the management of runners with PFP, recent research suggested that various interventions can be considered to help symptoms, even if these interventions target biomechanical factors that may not have caused the injury in the first place. In this Current Concepts article, we describe how the latest evidence on education about training modifications, strengthening exercises, gait and footwear modifications, and psychosocial factors can be applied when treating runners with PFP. The importance of maintaining relative homeostasis between load and capacity will be emphasized. Recommendations for temporary or longer-term interventions will be discussed. A holistic, evidence-based approach should consist of a graded exposure to load, including movement, exercise, and running, while considering the capacity of the individual, including sleep and psychosocial factors. Cost, accessibility, and the personal preferences of patients should also be considered."  
+              }, 
+            ]
+          },
+          {
+            id: "IFwHYE",
+            title: "A Systematic Review of Clinical Practice Guidelines for Physical Therapist Management of Patellofemoral Pain",
+            timeOfRead: 2,
+            dateOfPublication: "03/02/2021",
+            link: "https://academic.oup.com/ptj/article/101/3/pzab021/6106268",
+            resume: [
+              {
+                paragraph: 0,
+                title: "Objective",
+                content: "The purpose of this study was to conduct a systematic review to evaluate clinical practice guidelines for the physical therapist management of patellofemoral pain."  
+              },
+              {
+                paragraph: 1,
+                title: "Methods",
+                content: "Five electronic databases (CINAHL, Embase, Medline, Psychinfo, Cochrane Library) were searched from January 2013 to October 2019. Additional search methods included searching websites that publish clinical practice guidelines containing recommendations for physical therapist management of patellofemoral pain. Characteristics of the guidelines were extracted, including recommendations for examination, interventions, and evaluation applicable to physical therapist practice. Quality assessment was conducted using the Appraisal of Guidelines for Research and Evaluation (AGREE) II instrument, applicability of recommendations to physical therapist practice was examined using the AGREE Recommendation Excellence instrument, and convergence of recommendations across guidelines was assessed."  
+              },
+              {
+                paragraph: 2,
+                title: "Results",
+                content: "Four clinical practice guidelines were included. One guideline evaluated as higher quality provided the most clinically applicable set of recommendations for examination, interventions, and evaluation processes to assess the effectiveness of interventions. Guideline-recommended interventions were consistent for exercise therapy, foot orthoses, patellar taping, patient education, and combined interventions and did not recommend the use of electrotherapeutic modalities. Two guidelines evaluated as higher quality did not recommend using manual therapy (in isolation), dry needling, and patellar bracing."  
+              },
+              {
+                paragraph: 3,
+                title: "Conclusion",
+                content: "Recommendations from higher-quality clinical practice guidelines may conflict with routine physical therapist management of patellofemoral pain. This review provides guidance for clinicians to deliver high-value physical therapist management of patellofemoral pain."
+              },
+              {
+                paragraph: 4,
+                title: "Impact",
+                content: "This review addresses an important gap between evidence and practice in the physical therapist management of patellofemoral pain."
+              },
+              {
+                paragraph: 5,
+                title: "Lay Summary",
+                content: "If you have kneecap pain, this review offers guidance for your physical therapist to provide examination processes, treatments, and evaluation processes that are recommended by high-quality guidelines."
+              }
+            ]
+          },
+          {
+            id: "MdjMUB",
+            title: "Patellofemoral Pain",
+            timeOfRead: 4,
+            dateOfPublication: "31/08/2019",
+            link: "https://www.jospt.org/doi/10.2519/jospt.2019.0302",
+            resume: [
+              {
+                paragraph: 0,
+                title: "AIM OF THE GUIDELINES",
+                content: "The Academy of Orthopaedic Physical Therapy of the American Physical Therapy Association (APTA), Inc has an ongoing effort to create evidence-based clinical practice guidelines (CPGs) for orthopaedic physical therapy management of patients with musculoskeletal impairments described in the World Health Organization’s International Classification of Functioning, Disability and Health (ICF).319"  
+              },
+              {
+                paragraph: 1,
+                title: "STATEMENT OF INTENT",
+                content: "These guidelines are not intended to be construed or to serveas a standard of medical care. Standards of care are determined on the basis of all clinical data available for an individual patient and are subject to change as scientific knowledge and technology advance and patterns of care evolve. These parameters of practice should be considered guidelines only. Adherence to them will not ensure a successful outcome in every patient, nor should they be construed as including all proper methods of care or excluding other acceptable methods of care aimed at the same results. The ultimate judgment regarding a particular clinical procedure or treatment plan must be made based on clinician experience and expertise in light of the clinical presentation of the patient, the available evidence, available diagnostic and treatment options, and the patient’s values, expectations, and preferences. However, we suggest that significant departures from accepted guidelines should be documented in the patient’s medical records at the time the relevant clinical decision is made." 
+              },
+              {
+                paragraph: 2,
+                title: "SCOPE AND RATIONALE OF THE GUIDELINE",
+                content: "Patellofemoral pain (PFP) is a common musculoskeletal related condition that is characterized by insidious onset of poorly defined pain, localized to the anterior retropatellar and/or peripatellar region of the knee.80 The onset of symptoms can be slow or acutely develop with a worsening of pain accompanying lower-limb loading activities (eg, squatting, prolonged sitting, ascending/descending stairs, jumping, or running, especially with hills).154,234,254 Symptoms can restrict participation in physical activity, sports, and work.74 Symptoms can recur and can persist for years.74 Patients with PFP symptoms frequently present to health care professionals for diagnosis and treatment.74,277 This CPG will allow physical therapists and other rehabilitation specialists to stay up to date with evolving PFP knowledge and practices, and help them to make evidence-based treatment decisions.166"  
+              }
+            ]
+          },
+        ]
+      },
+      {
+        id: "ShmhFk",
+        name: "LCA",
+        articles: [
+          {
+            id: "X9aK81",
+            title: "Physical Examination and Patellofemoral Pain Syndrome: an Updated Review",
+            timeOfRead: 3,
+            dateOfPublication: "29/10/2021",
+            link: "https://link.springer.com/article/10.1007/s12178-021-09730-7",
+            resume:[]
+          }
+        ],
+        treatment: [
+          {
+            id: "BJj9fG",
+            title: "RETORNO AO JOGO APÓS RECONSTRUÇÃO DO LIGAMENTO CRUZADO ANTERIOR",
+            timeOfRead: 3,
+            dateOfPublication: "18/06/2020",
+            link: "https://www.scielo.br/j/rbme/a/fXnqrrFhyBX6pmQ4qYtfMSr/?lang=en",
+            resume:[
+              {
+                paragraph: 0,
+                title: "RESUMO",
+                content: "A ruptura do ligamento cruzado anterior (LCA) representa mais da metade de todas as lesões de joelho em esportes que envolvem rotações do corpo e mudanças repentinas de direção. Dar alta ao atleta para retorno ao jogo (RTP) após a reconstrução do LCA (ACLR) é uma tarefa difícil com responsabilidade multidisciplinar. Por muitos anos, um período de seis meses após a ACLR foi adotado como o único critério para RTP. No entanto, agora é sugerido que a RTP não deve ser exclusivamente baseada no tempo, mas em dados clínicos e avaliações sistemáticas. Apesar da importância dos fatores pós-ACLR para RTP, fatores pré e peri-ACLR também devem ser considerados. Historicamente, a ACLR é realizada com os tendões isquiotibiais ou patelares autólogos, embora a escolha do enxerto ainda seja um tema aberto e em constante evolução. A reconstrução do ligamento anterolateral e o reparo da ruptura da rampa meniscal associada à ACLR foram recentemente sugeridos como estratégias para melhorar a estabilidade da articulação do joelho. Questionários subjetivos são fáceis de aplicar e ajudam a identificar fatores físicos ou psicológicos que podem dificultar a RTP. Testes funcionais, como hop test e avaliação de força por meio de dinamômetros isocinéticos, são ferramentas fundamentais para tomada de decisão quando associados à avaliação clínica e à ressonância magnética. Recentemente, a capacidade de gerar força de forma explosiva foi incorporada à avaliação da força muscular. Isso é quantificado por meio da taxa de desenvolvimento de torque (RTD). Devido às características inerentes à prática esportiva, há um tempo extremamente curto disponível para produzir força. Assim, o RTD parece representar melhor as demandas atléticas do que a avaliação da força máxima isoladamente. Esta revisão investiga os fatores pré, peri e pós-LCA estabelecidos na literatura e compartilha nossa prática clínica, que consideramos ser a melhor prática para RTP."
+              }
+            ]
+          },
+          {
+            id: "tzelAb",
+            title: "Reabilitação da reconstrução do LCA",
+            timeOfRead: 2,
+            dateOfPublication: "18/06/2020",
+            link: "https://www.scielo.br/j/rbme/a/fXnqrrFhyBX6pmQ4qYtfMSr/?lang=en",
+            resume:[
+              {
+                paragraph: 0,
+                title: "Contexto",
+                content: "A reconstrução do ligamento cruzado anterior (LCA) (ACLR) e a reabilitação pós-operatória continuam sendo um foco multidisciplinar em ambientes de pesquisa e clínicos. Pesquisas recentes sobre ACLR justificam um reexame das práticas atuais de reabilitação dos clínicos para otimizar os resultados clínicos surpreendentemente variáveis após ACLR e retorno ao esporte. O objetivo do artigo e das diretrizes atualizadas é usar evidências contemporâneas para revisitar sistematicamente nossas diretrizes de prática e validar nossos marcos clínicos com dados de nossa prática universitária."
+              },
+              {
+                paragraph: 1,
+                title: "Aquisição de evidências",
+                content: "Usando o mecanismo de busca PubMed, artigos que relataram sobre reabilitação e protocolos do LCA, diretrizes, tipo de enxerto, cicatrização e distensão, retorno ao esporte, considerações psicológicas e prevenção de lesões secundárias publicados de 1979 a 2020 foram identificados usando os termos de busca protocolos do LCA, diretrizes, reabilitação do LCA, enxerto do LCA, exercício de cadeia cinética aberta (CCA) do LCA e  exercício de cadeia cinética fechada (CCC), retorno ao esporte do LCA, fatores psicológicos do LCA e prevenção de lesões do LCA."
               }
             ]
           }
